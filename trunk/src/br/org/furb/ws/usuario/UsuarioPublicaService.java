18769d9e6e0b9b07105e6cb181461b0b96714dbc
reiinoldo@gmail.com <reiinoldo@gmail.com@1a9ef337-92a4-a7a9-4d0f-4b10bb2f9467>
2014-11-19 21:45:54 +0000

    Web Service Usuário

---
+++ trunk/src/br/org/furb/ws/usuario/UsuarioPublicaService.java
@@ -0,0 +1,11 @@
+package br.org.furb.ws.usuario;
+
+import javax.xml.ws.Endpoint;
+
+
+public class UsuarioPublicaService {
+	public static void main(String[] args){
+		UsuarioWS service = new UsuarioWS();
+		Endpoint endpoint = Endpoint.publish("http://localhost:8080/usuario", service);
+	}
+}
