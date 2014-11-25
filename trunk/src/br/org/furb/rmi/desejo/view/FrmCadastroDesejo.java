31819d0eb1f6ea5e23a4bb00106b9d406a9366e5
andrevbampi@gmail.com <andrevbampi@gmail.com@1a9ef337-92a4-a7a9-4d0f-4b10bb2f9467>
2014-11-25 23:16:19 +0000

    Iniciando os trabalhos

---
+++ trunk/src/br/org/furb/rmi/desejo/view/FrmCadastroDesejo.java
@@ -0,0 +1,50 @@
+package br.org.furb.rmi.desejo.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class FrmCadastroDesejo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmCadastroDesejo frame = new FrmCadastroDesejo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmCadastroDesejo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 546, 421);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCadastroDeDesejos = new JLabel("Cadastro de Desejos");
+		lblCadastroDeDesejos.setBounds(0, 0, 520, 14);
+		contentPane.add(lblCadastroDeDesejos);
+		
+		JLabel lblTtulo = new JLabel("T\u00EDtulo");
+		lblTtulo.setBounds(10, 68, 46, 14);
+		contentPane.add(lblTtulo);
+	}
+}
