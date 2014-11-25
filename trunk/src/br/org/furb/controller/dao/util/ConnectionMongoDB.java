56702ad9b3e54dc2f4d18f43d3291d8f31bab3a4
andrevbampi@gmail.com <andrevbampi@gmail.com@1a9ef337-92a4-a7a9-4d0f-4b10bb2f9467>
2014-11-25 23:45:36 +0000

    Adicionando as classes de conexão do MongoDB e seu driver, e a telinha de cadastro de Desejo.

---
+++ trunk/src/br/org/furb/controller/dao/util/ConnectionMongoDB.java
@@ -0,0 +1,48 @@
+package br.org.furb.controller.dao.util;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Maicon
+ */
+public class ConnectionMongoDB {
+
+    private static final String HOST = "192.168.25.29"; //"localhost";
+    private static final String DB_NAME = "venda_rapida";
+    private static final int PORT = 27017;
+
+    public static DB getConnection() throws UnknownHostException {
+        Mongo m = new Mongo(HOST, PORT);
+        return m.getDB(DB_NAME);
+    }
+
+    public static void closeConnection(DB connection) {
+        if (connection != null) {
+            connection.getMongo().close();
+        }
+    }
+
+    public static int nextId(DBCollection collection, String idFieldName) throws SQLException {
+        DBObject sort = new BasicDBObject();
+        sort.put(idFieldName, -1);
+
+        DBCursor cursor = collection.find().sort(sort).limit(1);
+        try {
+            if (cursor.hasNext()) {
+                Integer valor = (Integer) cursor.next().get(idFieldName);
+                return valor+1;
+            }
+            return 1;
+        } finally {
+            cursor.close();
+        }
+    }
+}
