893a7f64f1b97389c00f84f3e7f0c16a9acf554e
reiinoldo <reiinoldo@gmail.com>
2014-11-26 21:18:40 +0000

    New ip

--- src/br/org/furb/ws/usuario/cliente/UsuarioWSService.java
+++ src/br/org/furb/ws/usuario/cliente/UsuarioWSService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.2
  * 
  */
-@WebServiceClient(name = "UsuarioWSService", targetNamespace = "http://usuario.ws.furb.org.br/", wsdlLocation = "http://localhost/usuario?wsdl")
+@WebServiceClient(name = "UsuarioWSService", targetNamespace = "http://usuario.ws.furb.org.br/", wsdlLocation = "http://192.168.25.11/usuario?wsdl")
 public class UsuarioWSService
     extends Service
 {
@@ -30,7 +30,7 @@ public class UsuarioWSService
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("http://localhost/usuario?wsdl");
+            url = new URL("http://192.168.25.11/usuario?wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
--- start_WS.bat
+++ start_WS.bat
@@ -3,5 +3,5 @@ cd workspace\EuQUero\src\
 set path=%path%;"C:\Arquivos de programas\Java\jdk1.7.0_25\bin"
 apt br\org\furb\ws\usuario\UsuarioWS.java
 pause
-wsimport -keep -p br.org.furb.ws.usuario.cliente http://localhost/usuario?wsdl
+wsimport -keep -p br.org.furb.ws.usuario.cliente http://192.168.25.11/usuario?wsdl
 pause
