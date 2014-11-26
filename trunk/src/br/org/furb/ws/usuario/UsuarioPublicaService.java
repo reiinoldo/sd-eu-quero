edbf19dc9c2aa8b5bb20605931ccc60a2ef52031
reiinoldo@gmail.com <reiinoldo@gmail.com@1a9ef337-92a4-a7a9-4d0f-4b10bb2f9467>
2014-11-26 21:18:40 +0000

    New ip

--- trunk/src/br/org/furb/ws/usuario/UsuarioPublicaService.java
+++ trunk/src/br/org/furb/ws/usuario/UsuarioPublicaService.java
@@ -6,6 +6,6 @@ import javax.xml.ws.Endpoint;
 public class UsuarioPublicaService {
 	public static void main(String[] args){
 		UsuarioWS service = new UsuarioWS();
-		Endpoint endpoint = Endpoint.publish("http://localhost/usuario", service);
+		Endpoint endpoint = Endpoint.publish("http://192.168.25.11/usuario", service);
 	}
 }
