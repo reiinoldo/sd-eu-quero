03495b0579210a400187ed394ef74d4ea0390580
andrevbampi@gmail.com <andrevbampi@gmail.com@1a9ef337-92a4-a7a9-4d0f-4b10bb2f9467>
2014-11-29 13:45:11 +0000

    Criação de desejo via RMI funcionando

--- trunk/src/br/org/furb/rmi/desejo/DesejoRMI.java
+++ trunk/src/br/org/furb/rmi/desejo/DesejoRMI.java
@@ -2,8 +2,10 @@ package br.org.furb.rmi.desejo;
 
 import java.rmi.*;
 
+import br.org.furb.model.Desejo;
+
 public interface DesejoRMI extends Remote{
 
-   public String CriarDesejo() throws RemoteException;
+   public String CriarDesejo(Desejo desejo) throws RemoteException;
 
 }
--- trunk/src/br/org/furb/rmi/desejo/DesejoServer.java
+++ trunk/src/br/org/furb/rmi/desejo/DesejoServer.java
@@ -1,15 +1,22 @@
 package br.org.furb.rmi.desejo;
 
+import java.net.MalformedURLException;
 import java.rmi.*;
 import java.rmi.server.*;
 
+import br.org.furb.controller.dao.DAO;
+import br.org.furb.controller.dao.impl.DesejoDaoImpl;
+import br.org.furb.model.Desejo;
 import br.org.furb.util.Constantes;
 
 @SuppressWarnings("serial")
 public class DesejoServer extends UnicastRemoteObject implements DesejoRMI{
 
-	protected DesejoServer() throws RemoteException {
+	DAO<Desejo> desejoDaoImpl;
+	
+	protected DesejoServer() throws RemoteException, MalformedURLException, NotBoundException {
 		super();
+		desejoDaoImpl = new DesejoDaoImpl();
 	}
 	
 	public static void main(String[] args) {
@@ -23,7 +30,14 @@ public class DesejoServer extends UnicastRemoteObject implements DesejoRMI{
 	
 
 	@Override
-	public String CriarDesejo() throws RemoteException {
+	public String CriarDesejo(Desejo desejo) throws RemoteException {
+		try {
+			if (desejo.getIdDesejo() == 0)
+				desejo.setIdDesejo(desejoDaoImpl.incrementar());
+			desejoDaoImpl.salvar(desejo);
+		} catch (Exception e) {
+			return e.getMessage();
+		}
 		return "Desejo criado com sucesso";
 	}
 	
