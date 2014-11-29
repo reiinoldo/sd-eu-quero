c36b5c4fa86ddf1d874e8c4af1a77debb594fca7
andrevbampi@gmail.com <andrevbampi@gmail.com@1a9ef337-92a4-a7a9-4d0f-4b10bb2f9467>
2014-11-29 20:27:20 +0000

    Implementados a Edição e a Exclusão dos Desejos e dos Pedidos

--- trunk/src/br/org/furb/rmi/oferta/OfertaRMI.java
+++ trunk/src/br/org/furb/rmi/oferta/OfertaRMI.java
@@ -10,4 +10,6 @@ public interface OfertaRMI extends Remote {
    public void criarOferta(Oferta oferta) throws Exception;
    public List<Oferta> listar(Oferta ofertaInicial, Oferta ofertaFinal) throws Exception;
    public Oferta buscar(Oferta oferta) throws Exception;
+   public boolean excluir(Oferta oferta) throws Exception;
+   public boolean editar(Oferta oferta) throws Exception;
 }
